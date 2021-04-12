291bdd840c27ac8f951dc8e4deb391bc30bf8fc1
Yoda <YodaPY@github.com>
2021-04-12 15:33:26 +0200

    0.0.8, actual plugin support

--- vexilux/commands.py
+++ vexilux/commands.py
@@ -3,8 +3,53 @@ import typing
 import inspect
 from lightbulb import get_command_signature, Context, WrappedArg
 from lightbulb import Command as _Command, Group as _Group
+from lightbulb.commands import _CommandT, _BoundCommandMarker
 
 T_converter = typing.Callable[[typing.Union[str, WrappedArg]], typing.Any]
+
+def _bind_prototype(instance: typing.Any, command_template: _CommandT):
+
+    class BoundCommand(type(command_template), _BoundCommandMarker):
+        def __init__(self) -> None:
+            _BoundCommandMarker.__init__(self, command_template)
+            self._delegates_to = command_template
+            self.__dict__.update(command_template.__dict__)
+
+        def __hash__(self) -> int:
+            return hash(self.name)
+
+        def __eq__(self, other) -> bool:
+            return isinstance(other, (type(self), Command)) and other.name == self.name
+
+        async def invoke(self, context: Context, *args: str, **kwargs: str) -> typing.Any:
+            if self.cooldown_manager is not None:
+                self.cooldown_manager.add_cooldown(context)
+            new_args = await self._convert_args(context, args, list(self.arg_details.args.values())[2 : len(args) + 2])
+
+            if kwargs and not self.flags:
+                new_kwarg = (
+                    await self._convert_args(
+                        context, kwargs.values(), [self.arg_details.args[self.arg_details.kwarg_name]]
+                    )
+                )[0]
+                kwargs = {self.arg_details.kwarg_name: new_kwarg}
+
+            return await self._callback(instance, context, *new_args, **kwargs)
+
+    prototype = BoundCommand()
+
+    instance.__dict__[command_template.method_name] = prototype
+
+    if isinstance(prototype, Group):
+        prototype._subcommands = {}
+        for subcommand_name, subcommand in command_template._subcommands.items():
+            for name, member in inspect.getmembers(instance, lambda m: isinstance(m, _BoundCommandMarker)):
+                if member.delegates_to is subcommand:
+                    member.parent = prototype
+                    prototype._subcommands[subcommand_name] = member
+                    prototype.subcommands.add(member)
+
+    return typing.cast(_CommandT, prototype)
 
 @attr.s
 class FlagDetails:
@@ -21,6 +66,9 @@ class Command(_Command):
         self.flags = []
 
         super().__init__(*args, **kwargs)
+
+    def __get__(self: _CommandT, instance: typing.Any, owner: typing.Type[typing.Any]) -> _CommandT:
+        return _bind_prototype(instance, self)
     
     @property
     def signature(self) -> str:
@@ -67,6 +115,12 @@ class Command(_Command):
         arg_details = list(self.arg_details.args.values())[1 : len(args) + 1]
         new_args = await self._convert_args(context, args[: len(arg_details)], arg_details)
         new_args = [*new_args, *args[len(arg_details) :]]
+
+        if kwargs and not self.flags:
+            new_kwarg = (
+                await self._convert_args(context, kwargs.values(), [self.arg_details.args[self.arg_details.kwarg_name]])
+            )[0]
+            kwargs = {self.arg_details.kwarg_name: new_kwarg}
 
         return await self._callback(context, *new_args, **kwargs)
 
